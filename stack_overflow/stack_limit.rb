ac478b6a8f44c66965b2c40eb6103068b3e06b7b
Ed Howland <ed.howland@gmail.com>
2018-08-26 18:05:08 -0400

    final version of stack_limit.rb

--- stack_overflow/stack_limit.rb
+++ stack_overflow/stack_limit.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/env ruby
 # stack_limit.rb - estimate the max stack depth
 
-def eternal
-  eternal
+def eternal &blk
+  yield 1
+  eternal(&blk)
 end
 
+limit = 0
 begin
-  eternal
+  eternal {|x| limit += x }
 rescue SystemStackError => e
   puts 'In SystemStackError'
-  puts '__ the maximum depth is:'
-    puts "__ #{e.backtrace.length}"
+  puts 'the maximum depth is:'
+    puts "#{e.backtrace.length}"
 
 rescue => err
-#  puts err.message
-  puts "__ #{err.class.name}"
-  puts '__ the maximum depth is:'
-    puts "__ #{err.backtrace.length}"
-    
+  puts "some other error occured: #{err.class.name}"
 end
+
+
+puts "limit: #{limit}"
