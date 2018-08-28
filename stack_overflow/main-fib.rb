d86796e304f7f93fc7b70a69f8f02c57c8e66f8e
Ed Howland <ed.howland@gmail.com>
2018-08-28 11:54:07 -0400

    added main drivers for fib, fib-memo.rb

---
+++ stack_overflow/main-fib.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# main-fib.rb - Main driver for fib.rb
+
+require_relative 'fib'
+
+
+x = ARGV.first
+puts fib x.to_i
+
---
+++ stack_overflow/main-memo.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# main-memo.rb - Main driver for fib-memo.rb
+
+require_relative 'fib-memo'
+
+
+x = ARGV.first
+puts fib x.to_i
+
